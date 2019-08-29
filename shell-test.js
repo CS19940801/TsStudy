4196eac644a71126adc977febc505b11d12233c2
chenshuai <541268303@qq.com>
2019-08-29 11:22:54 +0800

    修改

--- shell-test.js
+++ shell-test.js
@@ -1,20 +1,21 @@
 const shell = require('shelljs');
 
-// if (!shell.which('node') || !shell.which('npm')) {
-//     shell.echo('无法使用node&npm','请查看node&npm安装')
-// } else {
-//     shell.echo('npm安装正常，开始更新代码');
-//     shell.exec('npm install');
-// }
-
-if(!shell.which('git')) {
-    shell.echo('无法使用git','请查看git安装');
-}else{
-    shell.echo('git安装正常，准备提交代码');
-    shell.exec('git add .')
-    shell.exec('git commit -am "Auto-commit1"')
-    shell.exec('git push')
-    console.log('看看code', shell.exec('git commit -am "Auto-commit"').code);
+if (!shell.which('node') || !shell.which('npm')) {
+    shell.echo('无法使用node&npm','请查看node&npm安装')
+} else {
+    shell.echo('npm安装正常，开始更新代码');
+    shell.exec('npm install');
+    shell.exec('npm run build');
 }
+
+// if(!shell.which('git')) {
+//     shell.echo('无法使用git','请查看git安装');
+// } else {
+//     shell.echo('git安装正常，准备提交代码');
+//     shell.exec('git add .')
+//     shell.exec('git commit -am "Auto-commit1"')
+//     shell.exec('git push')
+//     console.log('看看code', shell.exec('git commit -am "Auto-commit"').code);
+// }
 
 
