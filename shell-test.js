f25a3945a8d6844cfea2e928156b4610abca8933
chenshuai <541268303@qq.com>
2019-08-29 10:32:54 +0800

    Auto-commit

--- shell-test.js
+++ shell-test.js
@@ -1,19 +1,20 @@
 const shell = require('shelljs');
 
-if (!shell.which('npm')) {
-    shell.echo('无法使用npm')
-} else {
-    shell.echo('npm安装正常，开始更新代码');
-    shell.exec('npm install');
-}
+// if (!shell.which('node') || !shell.which('npm')) {
+//     shell.echo('无法使用node&npm','请查看node&npm安装')
+// } else {
+//     shell.echo('npm安装正常，开始更新代码');
+//     shell.exec('npm install');
+// }
 
 if(!shell.which('git')) {
-    shell.echo('无法使用git');
+    shell.echo('无法使用git','请查看git安装');
 }else{
     shell.echo('git安装正常，准备提交代码');
     shell.exec('git add .')
     shell.exec('git commit -am "Auto-commit"')
-    console.log(shell.exec('git commit -am "Auto-commit"').code);
+    shell.exec('git push')
+    console.log(shell.exec('git commit -am "Auto-commit"').code,'看看code');
 }
 
 
