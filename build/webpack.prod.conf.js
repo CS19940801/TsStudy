385148234394f82fb07cf2b9a97acf6f54bb675f
chenshuai <541268303@qq.com>
2019-08-29 10:11:27 +0800

    Auto-commit

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,11 +1,4 @@
 'use strict'
-// const { CleanWebPackPlugin } = require('clean-webpack-plugin');
-
-// module.exports = {
-//   plugin: [
-//     new CleanWebPackPlugin()
-//   ]
-// }
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
